62471674b6dc9cf3d45fa1f8aaed97e2fce4e78f
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-01 16:49:05 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: Ia4fdfcbb0bb712a6daaa260734e9127fd2c295f7

--- WindowsAzureStorage.php
+++ WindowsAzureStorage.php
@@ -12,10 +12,11 @@ $wgExtensionCredits['other'][] = array(
 	'name'           => 'WindowsAzureStorage',
 	'author'         => array( '[http://www.hallowelt.biz Hallo Welt! Medienwerkstatt GmbH]', 'Markus Glaser', 'Robert Vogel' ),
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:WindowsAzureStorage',
-	'version'        => '1.0.0',
+	'version'        => '1.1.0',
 	'descriptionmsg' => 'windowsazurestorage-desc',
 );
 
+$wgMessagesDirs['WindowsAzureStorage'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['WindowsAzureStorage'] = $dir . 'WindowsAzureStorage.i18n.php';
 
 $wgAutoloadClasses['WindowsAzureFileBackend'] = $dir . 'WindowsAzureFileBackend.php';
